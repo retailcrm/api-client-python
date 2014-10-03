1ee23f40c906a3b5a97eedf619d229d256c00f93
Alex Lushpai <gwinn@users.noreply.github.com>
2014-10-04 03:59:12 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,11 @@
 from Intaro import IntaroApy
 
-# copy-one.ru
-crm = IntaroApy('https://copy-one.intarocrm.ru', 'Yogg9w7pxDW6sS3yESXljKpSueTJVURW')
-
-# copy-galaxys5
-#crm = IntaroApy('https://copy-one.intarocrm.ru', 'zpbJiKQEFEo9OlpEebM8OATs5t7dzgjB')
-
-# Pulwin
-#crm = IntaroApy('https://copy-one.intarocrm.ru', 'Lx2W5QRy6lQUotkaLd1cAXYRPIG4SdhR')
+crm = IntaroApy('hhttps://demo.intarocrm.ru', 'uLxXKBwjQteE9NkO3cJAqTXNwvKktaTc')
 
 order = {
-    'firstName': 'TESTER',
-    'phone': '89991119922',
+    'firstName': 'Intaro Test',
+    'phone': '+79000000000',
     'orderMethod': 'landing-page'
-  }
+}
 
 result = crm.orderCreate(order)
-
